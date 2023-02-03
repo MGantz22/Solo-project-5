1f9abce39341713d7fbd05b0530ce1c639018165
Mitchell Gantz <mgantz22@icloud.com>
2023-02-03 11:32:10 -0800

    create spaceage object

--- __tests__/spaceage.test.js
+++ __tests__/spaceage.test.js
@@ -1,1 +1,19 @@
-import SpaceAge from '../src/js/spaceage.js';
+import Spaceage from '../src/js/spaceage.js';
+
+describe('Spaceage', () => {
+
+    test("should correctly create a spaceage object with an age property", () => {
+        const spaceage = new Spaceage(32);
+        expect(spaceage.age).toEqual(32);
+    });
+
+
+
+
+
+
+    // test("should calculate age in earth years while on planet mercury", () => {
+    //     const spaceage = new Spaceage(32);
+    //     expect(spaceage.mercuryAgeCalc()).toEqual(133.3)
+    // });
+});
