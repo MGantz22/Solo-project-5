b3c701d0f5c120712381ff08f99c96286fd35758
Mitchell Gantz <mgantz22@icloud.com>
2023-02-05 08:29:57 -0800

    Edit code to simplify, first good fail.

--- __tests__/spaceage.test.js
+++ __tests__/spaceage.test.js
@@ -3,27 +3,27 @@ import Spaceage from '../src/js/spaceage.js';
 describe('Spaceage', () => {
 
     test("should correctly create a spaceage object with an age property", () => {
-        const spaceage = new Spaceage(32);
-        expect(spaceage.earthAge).toEqual(32);
+        const nowAge = new Spaceage(32);
+        expect(nowAge.nowAge).toEqual(32);
     });
 
-    test("should calculate age in earth years while on planet mercury", () => {
-        const spaceage = new Spaceage(32, 32);
-        expect(spaceage.mercuryAgeCalc()).toEqual(133.33.toFixed(2));
-    });
+    // test("should calculate age in earth years while on planet mercury", () => {
+    //     const spaceage = new Spaceage(32, 32);
+    //     expect(spaceage.mercuryAgeCalc()).toEqual(133.33.toFixed(2));
+    // });
 
-    test("should calculate age in earth years while on planet venus", () => {
-        const spaceage = new Spaceage(32, 32, 32);
-        expect(spaceage.venusAgeCalc()).toEqual(52.03.toFixed(2));
-    });
+    // test("should calculate age in earth years while on planet venus", () => {
+    //     const spaceage = new Spaceage(32, 32, 32);
+    //     expect(spaceage.venusAgeCalc()).toEqual(52.03.toFixed(2));
+    // });
 
-    test("should calculate age in earth years while on planet mars", () => {
-        const spaceage = new Spaceage(32, 32, 32, 32);
-        expect(spaceage.marsAgeCalc()).toEqual(17.02.toFixed(2));
-    });
+    // test("should calculate age in earth years while on planet mars", () => {
+    //     const spaceage = new Spaceage(32, 32, 32, 32);
+    //     expect(spaceage.marsAgeCalc()).toEqual(17.02.toFixed(2));
+    // });
 
-    test("should calculate age in earth years while on planet jupiter", () => {
-        const spaceage = new Spaceage(32, 32, 32, 32, 32);
-        expect(spaceage.jupiterAgeCalc()).toEqual(2.67.toFixed(2));
-    });
+    // test("should calculate age in earth years while on planet jupiter", () => {
+    //     const spaceage = new Spaceage(32, 32, 32, 32, 32);
+    //     expect(spaceage.jupiterAgeCalc()).toEqual(2.67.toFixed(2));
+    // });
 });
--- src/js/spaceage.js
+++ src/js/spaceage.js
@@ -1,29 +1,29 @@
 export default class Spaceage {
-    constructor(earthAge, mercuryAge, venusAge, marsAge, jupiterAge) {
-        this.earthAge = earthAge;
-        this.mercuryAge = mercuryAge;
-        this.venusAge = venusAge;
-        this.marsAge = marsAge;
-        this.jupiterAge = jupiterAge;
+    constructor(nowAge){ //pastAge, futureAge) {
+        this.nowhAge = nowAge;
+        // this.mercuryAge = mercuryAge;
+        // this.venusAge = venusAge;
+        // this.marsAge = marsAge;
+        // this.jupiterAge = jupiterAge;
 }
 
     mercuryAgeCalc() {
-        let mercuryAge = this.earthAge / .24;
-        return mercuryAge.toFixed(2);
+        // let mercuryAge = this.nowAge / .24;
+        // return mercuryAge.toFixed(2);
 }
 
-    venusAgeCalc() {
-        let venusAge = this.earthAge / .615;
-        return venusAge.toFixed(2);
-}
+//     venusAgeCalc() {
+//         let venusAge = this.nowAge / .615;
+//         return venusAge.toFixed(2);
+// }
 
-    marsAgeCalc() {
-        let marsAge = this.earthAge / 1.88;
-        return marsAge.toFixed(2);
-}
+//     marsAgeCalc() {
+//         let marsAge = this.nowAge / 1.88;
+//         return marsAge.toFixed(2);
+// }
 
-    jupiterAgeCalc() {
-        let jupiterAge = this.earthAge / 12;
-        return jupiterAge.toFixed(2);
-}
+//     jupiterAgeCalc() {
+//         let jupiterAge = this.nowAge / 12;
+//         return jupiterAge.toFixed(2);
+// }
 };
